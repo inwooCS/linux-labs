f6cf193d2780eaf34e0329a4f7f095f282eb239d
inwoo <inwoo@hawaii.edu>
2023-01-31 00:18:34 -1000

    Added comments to pingsweep.sh

--- bash-scripts/7.ping-sweep/pingsweep.sh
+++ bash-scripts/7.ping-sweep/pingsweep.sh
@@ -7,6 +7,7 @@ read -p "Please enter the subnet: " SUBNET
 
 # User specifies the subnet (e.g., 192.168.1 || 10.100.10)
 # The for loop checks the IPS (e.g., 192.168.1.1 -> 192.168.1.254)
+# '-c' stands for count (e.g., '1'). man ping for more information.
 for IP in $(seq 1 254); do
 	ping -c 1 $SUBNET.$IP
 done
