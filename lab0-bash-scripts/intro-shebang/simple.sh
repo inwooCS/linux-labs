7ed4bedd298756467a3132f8cf5ce6387f1de313
inwoo <inwoo@hawaii.edu>
2023-01-30 22:56:58 -1000

    Started bash-scripting lab with hello world

---
+++ lab0-bash-scripts/intro-shebang/simple.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+echo "Hello World!"
+
+# "#!"" is called "Shebang".
+# Everything after "#!/bin/" is the interpreter. 
+# The bash interpreter is considered to be a terminal instance.
+# The shell interpreter is the shell, the interpreter of the commands.
+# There are other interpreters available (e.g. #!/bin/python)
